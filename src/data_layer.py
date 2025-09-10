4bd9580bb0f4c88b77c57f7581fd8246e3bd08df
ishaan sangwan <ishaansangwan@gmail.com>
2025-09-11 03:13:53 +0530

    implement user authentication with password hashing and JWT support

--- src/data_layer.py
+++ src/data_layer.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey
 from pydantic import BaseModel
+from passlib.hash import sha256_crypt
 
 Base = declarative_base()
 
@@ -11,7 +12,21 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     name = Column(String)
     email = Column(String, unique=True, index=True)
+    _password = Column(String, nullable=False, server_default='not set')
+    description = Column(String)
+
+    @property
+    def password(self):
+        raise AttributeError("password is not a readable attribute")
+
+    @password.setter
+    def password(self, password):
+        self._password = sha256_crypt.hash(password)
+
+    def verify_password(self, password):
+        return sha256_crypt.verify(password, self._password)
 
 class UserCreate(BaseModel):
-    name: str
+    name: str = None
     email: str
+    password: str
--- src/main.py
+++ src/main.py
@@ -1,13 +1,23 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Header
+from sqlalchemy.sql.functions import current_user
+
 from data_layer import Base, User
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 import asyncio
-
+from sqlalchemy.exc import IntegrityError
+from fastapi import HTTPException
 from data_layer import UserCreate
+import os
+from sqlalchemy import select
+import jwt
+import datetime
 
+pg_address = os.environ.get('PG_ADDRESS', '192.168.122.53:5432')
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 engine = create_async_engine(
-    'postgresql+asyncpg://postgres:q123@192.168.122.53:5432/testing'
+    f'postgresql+asyncpg://postgres:q123@{pg_address}/testing'
 )
 
 async def get_session():
@@ -24,6 +34,21 @@ async def get_session():
     finally:
         await session.close()
 
+
+def validate_jwt(token: str):
+    try:
+        print(token)
+        payload = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+        id = payload.get('user_id')
+        return id
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail='Expired token')
+    except jwt.InvalidTokenError:
+        raise HTTPException(status_code=401, detail='Invalid Token')
+
+def get_token(authorization: str = Header('Authorization')):
+    token = authorization.replace('Bearer ', '')
+    return validate_jwt(token)
 app = FastAPI()
 
 @app.on_event("startup")
@@ -34,11 +59,57 @@ async def on_startup():
 
 @app.post("/signup")
 async def create_user(user_in: UserCreate,  session: AsyncSession = Depends(get_session)):
-    user = User(
-        name=user_in.name,
-        email=user_in.email,
-    )
-    session.add(user)
-    await session.commit()
-    await session.refresh(user)
-    return user
+    try:
+        user = User(
+            name=user_in.name,
+            email=user_in.email,
+        )
+        user.password = user_in.password
+        session.add(user)
+        await session.commit()
+        await session.refresh(user)
+        return user
+    except IntegrityError as error:
+        await session.rollback()
+        raise HTTPException(status_code=409, detail=f'the user email already exists {user_in.email}')
+    except Exception as error:
+        await session.rollback()
+        raise HTTPException(status_code=500, detail=str(error))
+
+
+@app.get("/users")
+async def get_users(session: AsyncSession = Depends(get_session), current_user=Depends(get_token)):
+    try:
+        query = select(User)
+        result = await session.execute(query)
+        users = result.scalars().all()
+        return users
+    except Exception as error:
+        pass
+
+@app.post('/login')
+async def login(user_in: UserCreate,session: AsyncSession = Depends(get_session)):
+    try:
+        query = select(User).where(User.email == user_in.email)
+        result = await session.execute(query)
+        result = result.scalars().first()
+        if result is None:
+            return {
+                "error":'no user found'
+            }
+
+        if result.verify_password(user_in.password):
+
+            payload = {
+                'user_id': result.id,
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30),
+                'iat':datetime.datetime.utcnow()
+            }
+            token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+            return {
+                "access":token
+            }
+        else:
+            return False
+    except Exception as error:
+        raise HTTPException(status_code=500, detail=str(error))
